b30daff370f0e9f1cbea37895261683bf4e5c4a1
Mohamed Almatry <moalmatry2000@gmail.com>
2025-03-26 22:13:12 +0200

    feat: add thank you page

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,10 @@
+import ThankYou from "@/components/ThankYou";
+import React, { Suspense } from "react";
+
+export default function ThankYouPage() {
+  return (
+    <Suspense>
+      <ThankYou />
+    </Suspense>
+  );
+}
---
+++ src/components/ThankYou/actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export const getPaymentStatus = async ({ orderId }: { orderId: string }) => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user.id || !user.email) throw new Error("Must be logged in");
+
+  const order = await db.order.findFirst({
+    where: { id: orderId, userId: user.id },
+    include: {
+      billingAddress: true,
+      configuration: true,
+      shippingAddress: true,
+      user: true,
+    },
+  });
+
+  if (!order) throw new Error("Order not found");
+
+  if (order.isPaid) return order;
+  else return false;
+};
